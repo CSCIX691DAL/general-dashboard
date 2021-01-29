73f0e62d952303de461aca58e4cf0a435660908d
Dajel Bourque <dj654130@dal.ca>
2021-01-29 18:56:54 -0400

    Register button routes to login. Validation still required

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  onClick(): void{
+    // Put validation checks here
+    const bool = true;
+    // If validation checks pass then route user to login page
+    if (bool){
+      this.router.navigate(['/login']).then(response => {
+        console.log(response);
+      });
+    }
+    else{
+      alert('Incorrect info');
+    }
+  }
 }
