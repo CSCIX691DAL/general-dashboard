02588ddd9253510a2a239c301d6185f24e8f0a7d
Dajel Bourque <dj654130@dal.ca>
2021-01-31 10:34:42 -0400

    Create simple form validation (checks if fields are empty) with validForm() method, more validation should be added

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -12,17 +12,44 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  /**
+   * @desc Triggers when register button is clicked on Registration page
+   * determines whether or not to route user to Login page by calling validForm(form) method
+   * and redirects user based on the returned boolean
+   */
   onClick(): void{
-    // Put validation checks here
-    const bool = true;
+    // Add other validation checks to validForm() method (below this method)
+    const form = document.getElementById('registerForm');
+    const bool = this.validForm(form);
     // If validation checks pass then route user to login page
     if (bool){
       this.router.navigate(['/login']).then(response => {
         console.log(response);
       });
     }
+    // Display some sort of error message, for now it will display the required fields auto message
     else{
-      alert('Incorrect info');
     }
   }
+
+  /**
+   * @desc Called by the onClick method to check whether or not the registration form
+   * has valid data in each input field (checks if Field is empty for now add more validation criteria
+   * to this method).
+   * @param form - the form element in registration.component.html which contains all the input fields
+   */
+  validForm(form): boolean{
+    // Add form validation criteria here
+    const inputFields = form.getElementsByTagName('input');
+    for (let i = 0; i < inputFields.length; i++){
+      if (inputFields[i].hasAttribute('required')){
+        if (inputFields[i].value === ''){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
 }
