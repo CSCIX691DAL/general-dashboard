4b308857fee265974bc4e12f090b46e80e4866c4
Dajel Bourque <dj654130@dal.ca>
2021-02-03 16:41:22 -0400

    Successfully refactored all html and ts files for registration.component to follow angular/forms standard

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -24,6 +24,20 @@ export class RegistrationComponent implements OnInit {
   });
 
   /**
+   * @desc This will take the user input in password and confirm password fields and checks it against the corresponding
+   * lengths and patterns
+   */
+  passwords = new FormGroup({password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(12),
+      Validators.pattern(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/),
+      Validators.pattern(/\d/),
+      Validators.pattern(/[a-zA-Z]/)
+    ]), confirmPassword: new FormControl('', [
+      Validators.required,
+    ])});
+  /**
    * This allows the html file to call the users
    * email to check its status in the form
    */
@@ -31,81 +45,39 @@ export class RegistrationComponent implements OnInit {
   getUserEmail(){
     return this.userEmail.get('email');
   }
+  /**
+   * This allows the html file to call the users
+   * password to check its status in the form
+   */
+  // tslint:disable-next-line:typedef
+  getUserPassword(){
+    return this.passwords.get('password');
+  }
+  /**
+   * This allows the html file to call the confirm
+   * password to check its status in the form
+   */
+  // tslint:disable-next-line:typedef
+  getConfirmPassword(){
+    return this.passwords.get('confirmPassword');
+  }
 
   ngOnInit(): void {
   }
 
   /**
    * @desc Triggers when register button is clicked on Registration page
-   * determines whether or not to route user to Login page by calling validForm(form) method
-   * and redirects user based on the returned boolean
+   * determines whether or not to route user to Login page by ensuring all form input
+   * is validated
    */
   onClick(): void{
-    // Add other validation checks to validForm() method (below this method)
-    const form = document.getElementById('registerForm');
-    const bool = this.validForm(form);
-    const boolPW = this.validPassword(form);
-    // If validation checks pass then route user to login page
-    if (bool && boolPW){
+    if (this.getUserPassword().valid && this.getUserEmail().valid && this.getUserPassword().value === this.getConfirmPassword().value){
       this.router.navigate(['/login']).then(response => {
         console.log(response);
       });
     }
-    // Display some sort of error message, for now it will display the required fields auto message
     else{
+      alert('Expected something different, please try again.');
     }
   }
-
-  /**
-   * @desc Called by the onClick method to check whether or not the registration form
-   * has valid data in each input field and also checking if password inputs match (checks if Field is
-   * empty for now add more validation criteria to this method).
-   * @param form - the form element in registration.component.html which contains all the input fields
-   */
-  validForm(form): boolean {
-    // Add form validation criteria here
-    const inputFields = form.getElementsByTagName('input');
-
-    for (let i = 0; i < inputFields.length; i++) {
-      if (inputFields[i].hasAttribute('required')) {
-        if (inputFields[i].value === '') {
-          return false;
-        }
-        // Password match check
-        if (i === 2) {
-          const password = inputFields[i - 1].value;
-          if (password !== inputFields[i].value) {
-            alert('Passwords do not match');
-            return false;
-          }
-        }
-      }
-    }
-    return true;
-  }
-  validPassword(form): boolean{
-    const inputFields = form.getElementsByTagName('input');
-    const specialChars  = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-    const numChars = /\d/;
-    const letterChars = /[a-zA-Z]/;
-
-    if (4 > inputFields[1].value.length || inputFields[1].value.length > 12) {
-      alert('Password must be between 4 and 12 characters long');
-      return false;
-    }
-    if (!(specialChars.test(inputFields[1].value)) ) {
-      alert('Password must contain at least one special character');
-      return false;
-    }
-    if (!(numChars.test(inputFields[1].value))) {
-      alert('Password must contain at least one number');
-      return false;
-    }
-    if (!(letterChars.test(inputFields[1].value))) {
-      alert('Password must contain at least one letter');
-      return false;
-    }
-    return true;
-  }
-
 }
