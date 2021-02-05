84dea34e9ad403e2529f4de7ea17605d4eee5053
Dajel Bourque <dj654130@dal.ca>
2021-02-05 13:38:29 -0400

    Error message presented upon use of existing email on registration page

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -11,7 +11,8 @@ import {DatabaseService} from '../services/database-connection.service';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor(private router: Router, private conn: DatabaseService) { }
+  constructor(private router: Router, private conn: DatabaseService) {
+  }
 
   /**
    * @desc This is what will take the users input and checks it against the pattern
@@ -21,14 +22,15 @@ export class RegistrationComponent implements OnInit {
     email: new FormControl('', [
       Validators.required,
       Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}')
-      ]),
+    ]),
   });
 
   /**
    * @desc This will take the user input in password and confirm password fields and checks it against the corresponding
    * lengths and patterns
    */
-  passwords = new FormGroup({password: new FormControl('', [
+  passwords = new FormGroup({
+    password: new FormControl('', [
       Validators.required,
       Validators.minLength(4),
       Validators.maxLength(12),
@@ -37,29 +39,37 @@ export class RegistrationComponent implements OnInit {
       Validators.pattern(/[a-zA-Z]/)
     ]), confirmPassword: new FormControl('', [
       Validators.required,
-    ])});
+    ])
+  });
+  /**
+   * @desc Hides div which displays error message if user exists in database
+   */
+  isVisible = false;
+
   /**
    * This allows the html file to call the users
    * email to check its status in the form
    */
   // tslint:disable-next-line:typedef
-  getUserEmail(){
+  getUserEmail() {
     return this.userEmail.get('email');
   }
+
   /**
    * This allows the html file to call the users
    * password to check its status in the form
    */
   // tslint:disable-next-line:typedef
-  getUserPassword(){
+  getPassword() {
     return this.passwords.get('password');
   }
+
   /**
    * This allows the html file to call the confirm
    * password to check its status in the form
    */
   // tslint:disable-next-line:typedef
-  getConfirmPassword(){
+  getConfirmPassword() {
     return this.passwords.get('confirmPassword');
   }
 
@@ -69,17 +79,27 @@ export class RegistrationComponent implements OnInit {
   /**
    * @desc Triggers when register button is clicked on Registration page
    * determines whether or not to route user to Login page by ensuring all form input
-   * is validated
+   * is valid. Then calls 'existingUser' method to check email against database which
+   * determines whether or not to create new user.
    */
-  onClick(): void{
-    if (this.getUserPassword().valid && this.getUserEmail().valid && this.getUserPassword().value === this.getConfirmPassword().value){
-      this.createUser(this.getUserEmail().value, this.getUserPassword().value);
-      this.router.navigate(['/login']).then(response => {
-        console.log(response);
+  // tslint:disable-next-line:typedef
+  validate(): void {
+    const emailInputTag = this.getUserEmail();
+    const passwordInputTag = this.getPassword();
+    const confirmPasswordInputTag = this.getConfirmPassword();
+    const emailString = emailInputTag.value;
+    const passwordString = passwordInputTag.value;
+    const confirmPasswordString = confirmPasswordInputTag.value;
+    // Ensures all form data is validated, if so calls existingUser to either create new user
+    // or notify user that this email is already in use by another user
+    if (passwordInputTag.valid && emailInputTag.valid &&
+      passwordString === confirmPasswordString) {
+      this.existingUser(emailString, passwordString).then(result => {
+        console.log(result);
       });
     }
     else{
-      alert('Expected something different, please try again.');
+      alert('Please fill all input fields according to specifications in red text');
     }
   }
   /**
@@ -94,4 +114,29 @@ export class RegistrationComponent implements OnInit {
     });
   }
 
+  /**
+   * @desc Checks a given email against the database. If no matching email is found, a new user is created.
+   * and password, and the user is redirected to login page. Otherwise, error message is presented.
+   * @param id string containing the email address given by the user in username input field of form on registration page
+   * @param password string containing the password given by the user in password input field of form on registration page
+   */
+  async existingUser(id, password): Promise<any>{
+    await new Promise(resolve => {
+      this.conn.getUser(id).subscribe(data => {
+        // If no match is found i.e. email not stored in database
+        console.log(data[0]);
+        if (data[0] == null){
+          this.createUser(id, password);
+          this.router.navigate(['/login']).then(response => {
+            console.log(response);
+          });
+        }
+        else{
+          // Display error message
+          this.isVisible = true;
+        }
+        resolve(data[0]);
+      });
+    });
+  }
 }
