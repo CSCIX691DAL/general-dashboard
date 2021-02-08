0944a27f04b6a46781858677875c1394d8e2fc2c
Adam Mattatall <ad260653@dal.ca>
2021-02-07 20:10:32 -0400

    Fixing broken tests

--- src/app/services/employees.service.spec.ts
+++ src/app/services/employees.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EmployeesService } from './employees.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('EmployeesService', () => {
   let service: EmployeesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(EmployeesService);
   });
 
