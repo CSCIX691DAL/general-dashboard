9573e3d100fcb4d0a94683ce81ea970643ba4bc4
Nick Fagan <nfagan@dal.ca>
2021-02-22 10:42:33 -0400

    Fix Tests

--- src/app/guest.guard.spec.ts
+++ src/app/guest.guard.spec.ts
@@ -2,11 +2,18 @@ import { TestBed } from '@angular/core/testing';
 
 import { GuestGuard } from './guest.guard';
 
+import { MockAuthService } from './mock-auth.service';
+import { AuthService } from './auth.service';
+
 describe('GuestGuard', () => {
   let guard: GuestGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+    providers: [
+	  { provide: AuthService, useClass: MockAuthService }
+	]
+	});
     guard = TestBed.inject(GuestGuard);
   });
 
