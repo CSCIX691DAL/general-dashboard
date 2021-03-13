a0a9c2fdb8a93d300dfc706f60d3f1a6708ca954
Adam Mattatall <ad260653@dal.ca>
2021-03-13 12:49:29 -0400

    add import for httpclienttestingmodule to fix pipeline error

--- src/app/services/reports.service.spec.ts
+++ src/app/services/reports.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ReportsService } from './reports.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('ReportsService', () => {
   let service: ReportsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+
+    });
     service = TestBed.inject(ReportsService);
   });
 
