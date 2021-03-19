b384ba24213f184d7358eb7dbd519284ace20187
Adam Mattatall <ad260653@dal.ca>
2021-03-19 18:44:06 -0300

    fixing some test errors

--- src/app/services/users.service.spec.ts
+++ src/app/services/users.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule]
+    });
     service = TestBed.inject(UsersService);
   });
 
