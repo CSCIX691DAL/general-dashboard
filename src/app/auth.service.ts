868c118c85ecbae58f98665fd320223b1ed54e48
Dajel Bourque <dj654130@dal.ca>
2021-03-24 11:15:25 -0300

    Bugfix for admin privilege expiring upon refresh

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {JSONObject} from "puppeteer";
+import {JSONObject} from 'puppeteer';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
-  bool = false;
+  bool;
+  // To fix admin refresh bug - gets called in ngOnInit() of navbar.component.ts
+  checkAdmin(): void{
+    if (this.hasToken()){
+      this.getAdmin();
+    }
+  }
   constructor(private Http: HttpClient) { }
 
   getToken(): string | null {
