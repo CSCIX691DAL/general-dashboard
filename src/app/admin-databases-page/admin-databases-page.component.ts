ba3f5498e7bbf4b4481f6bd7fd12aaf2cf3b81e5
RyanFischback <Ryan-Fischback@hotmail.com>
2021-03-30 19:27:56 -0300

    setup admin databases page displaying existing databases

--- src/app/admin-databases-page/admin-databases-page.component.ts
+++ src/app/admin-databases-page/admin-databases-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../auth.service';
 import {Router} from '@angular/router';
+import {DatabaseService} from '../services/database-connection.service';
+import {Database} from '../../models/database';
 
 @Component({
   selector: 'app-admin-databases-page',
@@ -9,13 +11,14 @@ import {Router} from '@angular/router';
 })
 export class AdminDatabasesPageComponent implements OnInit {
 
-  constructor(public auth: AuthService, private router: Router) {
+  dataBases: Database[];
+  constructor(public auth: AuthService, private router: Router, public conn: DatabaseService) {
     if (auth.isAdmin() === false){
       this.router.navigate(['/home']);
     }
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.dataBases = await this.conn.getDatabaseConnections();
   }
-
 }
