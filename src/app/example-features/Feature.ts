ffe8e26402410a595b66ac33f3d1ea84481ba898
Dajel Bourque <dj654130@dal.ca>
2021-04-11 12:07:02 -0300

    Replaced cat images with screenshots of website features

--- src/app/example-features/Feature.ts
+++ src/app/example-features/Feature.ts
@@ -2,26 +2,12 @@ export interface Feature{
   name: string;
   url: string;
 }
-export const Cats: Feature[] = [
-  {name: 'Feature1', url: '../assets/images/cat1.jpg'},
-  {name: 'Feature2', url: '../assets/images/cat2.jpg'},
-  {name: 'Feature3', url: '../assets/images/cat3.jpg'},
-  {name: 'Feature4', url: '../assets/images/cat1.jpg'},
-  {name: 'Feature5', url: '../assets/images/cat2.jpg'},
-  {name: 'Feature6', url: '../assets/images/cat3.jpg'},
-  {name: 'Feature7', url: '../assets/images/cat1.jpg'},
-  {name: 'Feature8', url: '../assets/images/cat2.jpg'},
-  {name: 'Feature9', url: '../assets/images/cat3.jpg'},
-  {name: 'Feature10', url: '../assets/images/cat1.jpg'},
-  {name: 'Feature11', url: '../assets/images/cat2.jpg'},
-  {name: 'Feature12', url: '../assets/images/cat3.jpg'},
-  {name: 'Feature13', url: '../assets/images/cat1.jpg'},
-  {name: 'Feature14', url: '../assets/images/cat2.jpg'},
-  {name: 'Feature15', url: '../assets/images/cat3.jpg'},
-  {name: 'Feature16', url: '../assets/images/cat1.jpg'},
-  {name: 'Feature17', url: '../assets/images/cat2.jpg'},
-  {name: 'Feature18', url: '../assets/images/cat3.jpg'},
-  {name: 'Feature19', url: '../assets/images/cat1.jpg'},
-  {name: 'Feature20', url: '../assets/images/cat2.jpg'},
-  {name: 'Feature21', url: '../assets/images/cat3.jpg'},
+export const Features: Feature[] = [
+  {name: 'Login Page', url: '../assets/images/Login.png'},
+  {name: 'Registration Page', url: '../assets/images/Registration.png'},
+  {name: 'User Homepage', url: '../assets/images/UserHome.png'},
+  {name: 'Generate Reports', url: '../assets/images/Reports.png'},
+  {name: 'Create New Reports (Admin)', url: '../assets/images/Admin (Reports).png'},
+  {name: 'Manage Databases (Admin)', url: '../assets/images/Admin (Databases).png'},
+  {name: 'Manage Users (Admin)', url: '../assets/images/Admin (Users).png'}
 ];
