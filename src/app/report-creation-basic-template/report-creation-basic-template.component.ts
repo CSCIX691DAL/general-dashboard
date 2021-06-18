60c2dad9f2a9684d72a669284fcf79a69f073ec8
hcui68 <hj858960@dal.ca>
2021-06-17 21:35:35 -0300

    fixing popup component issue v2

--- src/app/report-creation-basic-template/report-creation-basic-template.component.ts
+++ src/app/report-creation-basic-template/report-creation-basic-template.component.ts
@@ -38,12 +38,12 @@ export class ReportCreationBasicTemplateComponent implements OnInit {
   }
 
   open(content): void {
-    // reset variables
     this.modalService.open(content, {size: 'lg', ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
+    
   }
 
   async databaseChange(event: any): Promise<void>{
@@ -101,10 +101,24 @@ export class ReportCreationBasicTemplateComponent implements OnInit {
       this.selectedModel,
       this.selectedDatabase.id).then(resp => {
         alert('report has been created');
+        this.modalService.dismissAll();
+        this.resetModal();
     })
       .catch(err => {
       alert(err.msg);
     });
+    
+    
+  }
+
+  resetModal(): void{
+    this.selectedModel = ("");
+    this.selectedModelStructure = (null);
+    this.selectedFunc = ("");
+    this.selectedForFunc = ("");
+    this.selectedDatabase = (null);
+    this.reportDisplayName = ("");
+    this.reportName = ("");
   }
 
   private selectColumns(): string {
