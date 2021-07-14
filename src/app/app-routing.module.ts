3b91656145fca3113fdb32bcce1a2d3681a85c42
aubad <abusabiubada55@gmail.com>
2021-07-14 12:12:46 -0300

    Deleted some folders and cleaned up more code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { UserhomeComponent } from './userhome/userhome.component';
 import { GuestGuard } from './guest.guard';
 import { LoggedInGuard } from './logged-in.guard';
-import { AdminReportPageComponent } from './admin-report-page/admin-report-page.component';
 import { AdminUsersPageComponent } from './admin-users-page/admin-users-page.component';
 import { AdminDatabasesPageComponent } from './admin-databases-page/admin-databases-page.component';
-import { ReportsPageComponent } from './reports-page/reports-page.component';
+
 
 export const routes: Routes = [
   {
@@ -16,10 +15,8 @@ export const routes: Routes = [
     canActivate: [LoggedInGuard],
   },
   { path: 'home', component: HomeComponent },
-  { path: 'admin-report-page', component: AdminReportPageComponent },
   { path: 'admin-users-page', component: AdminUsersPageComponent },
   { path: 'admin-databases-page', component: AdminDatabasesPageComponent },
-  { path: 'reports', component: ReportsPageComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
